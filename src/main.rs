d760b1792731083e5e06a99ca487c3b8052c5536
Morgan Hester <morganhasebehester@gmail.com>
2022-12-03 13:07:40 -0800

    Added Dice struct to better organize code for future functionality

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,28 @@ fn help_menu() {
     println!("With 2 arguments, the first will be the number of dice to roll and the second the number of sides per die. \n`roll <number of dice> <type of die>\n");
 }
 
+struct Dice {
+    results: Vec<u32>,
+    die: u32,
+}
+
+impl Dice {
+    fn print_result(&self) {
+        if self.results.len() == 1 {
+            println!("Result: {}", self.results[0])
+        } else {
+            for i in 0..self.results.len() {
+                println!("Result {}: {}", i + 1, self.results[i]);
+            }
+            let mut sum: u32 = 0;
+            for i in 0..self.results.len() {
+                sum += self.results[i];
+            }
+            println!("Total of {} D{}: {}", self.results.len(), self.die, sum);
+        }
+    }
+}
+
 fn roll(die: u32) -> u32 {
     rand::thread_rng().gen_range(1..=die)
 }
@@ -26,22 +48,7 @@ fn roll_multiple(die: u32, num: u32) -> Vec<u32> {
     results
 }
 
-fn print_result(vec: Vec<u32>) {
-    if vec.len() == 1 {
-        println!("Result: {}", vec[0])
-    } else {
-        for i in 0..vec.len() {
-            println!("Result {}: {}", i + 1, vec[i]);
-        }
-        let mut sum: u32 = 0;
-        for i in 0..vec.len() {
-            sum += vec[i];
-        }
-        println!("Total of {} die: {}", vec.len(), sum);
-    }
-}
-
-fn no_args() -> Vec<u32> {
+fn no_args() -> Dice {
     println!("Number of dice to roll: ");
     let mut num = String::new();
     io::stdin().read_line(&mut num).expect("Failed to read input.");
@@ -70,20 +77,14 @@ fn no_args() -> Vec<u32> {
         process::exit(1);
     }
     
-    roll_multiple(die, num)
+    Dice {
+        results: roll_multiple(die, num),
+        die: die,
+    }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        print_result(no_args())
-    } else if args.len() == 2 {
-        if args[1] == String::from("help"){
-            help_menu();
-            process::exit(1);
-        }
-
-        let die: u32;
+fn one_arg(args: Vec<String>) -> Dice {
+    let die: u32;
         if let Ok(n) = args[1].trim().parse() {
             die = n;
         } else {
@@ -92,37 +93,60 @@ fn main() {
         }
         if die == 0 {
             println!("Cannot roll a 0 sided die.");
+            process::exit(1);
         } else {
-            let res = roll(die);
-            println!("Result: {}", res);
+            Dice {
+                results: roll_multiple(die, 1),
+                die: die,
+            }
         }
+}
+
+fn two_args(args: Vec<String>) -> Dice {
+    let die: u32;
+    let num: u32;
+
+    if let Ok(n) = args[2].trim().parse() {
+        die = n;
+    } else {
+        println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
+        process::exit(1);
+    }
+
+    if let Ok(n) = args[1].trim().parse() {
+        num = n;
+    } else {
+        println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
+        process::exit(1);
+    }
+    if num == 0 || die == 0 {
+        println!("Cannot roll 0 dice or a 0 sided die.");
+        process::exit(1);
+    }
+    if num > 2 {
+        println!("Rolling {} D{}s", num, die);
+    } else {
+        println!("Rolling 1 D{}", die);
+    }
+    Dice {
+        results: roll_multiple(die, num),
+        die: die,
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        no_args().print_result()
+    } else if args.len() == 2 {
+        if args[1] == String::from("help"){
+            help_menu();
+            process::exit(1);
+        }
+
+        one_arg(args).print_result();
     } else if args.len() == 3 {
-        let die: u32;
-        let num: u32;
-
-        if let Ok(n) = args[2].trim().parse() {
-            die = n;
-        } else {
-            println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
-            process::exit(1);
-        }
-
-        if let Ok(n) = args[1].trim().parse() {
-            num = n;
-        } else {
-            println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
-            process::exit(1);
-        }
-        if num == 0 || die == 0 {
-            println!("Cannot roll 0 dice or a 0 sided die.");
-            process::exit(1);
-        }
-        if num > 2 {
-            println!("Rolling {} D{}s", num, die);
-        } else {
-            println!("Rolling 1 D{}", die);
-        }
-        print_result(roll_multiple(die, num));
+        two_args(args).print_result();
     } else {
         println!("Usage: roll <type of die> <number of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
     }
