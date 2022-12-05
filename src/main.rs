5190c9b082aac597a83964e134ba6908d2581c70
Morgan Hester <morganhasebehester@gmail.com>
2022-12-05 10:58:33 -0800

    Used the clap crate to handle command line arguments and help menu

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use rand::Rng;
 use std::io;
 use std::process;
-use std::env;
+extern crate clap;
+use clap::Parser;
 
-fn help_menu() {
-    println!("This is a command line utility to quickly roll multiple (or single) dice and sum their values.");
-    println!("It has been designed mostly for use with role playing games like D&D, but can be used any time dice are used.");
-    println!("This was originally written in Go, but I wanted to rewrite and hopefully improve it in Rust.");
-    println!("\nUsage:\n");
-    println!("Can be run with no arguments, 1 argument or 2 arguments.");
-    println!("With no arguments, you will be asked to input the number of dice you with to roll and then the number of sides you want the die to have.\n");
-    println!("With 1 argument, only a single die will be rolled with the number of sides specified in the argument. \n`roll <type of die>`\n");
-    println!("With 2 arguments, the first will be the number of dice to roll and the second the number of sides per die. \n`roll <number of dice> <type of die>\n");
+#[derive(Parser)]
+#[command(name = "Rusty-Dice")]
+#[command(author = "Morgan Hester <morganhasebehester@gmail.com>")]
+#[command(about = "CLI dice roller", long_about = None)]
+struct Cli {
+    ///Optional argument for the number of sides each die has
+    die: Option<u32>,
+    #[arg(default_value_t = 1)]
+    ///Optional argument for the number of dice to roll
+    num: u32,
 }
 
 struct Dice {
@@ -83,71 +85,18 @@ fn no_args() -> Dice {
     }
 }
 
-fn one_arg(args: Vec<String>) -> Dice {
-    let die: u32;
-        if let Ok(n) = args[1].trim().parse() {
-            die = n;
-        } else {
-            println!("Usage: roll <type of die>\nArgument is optional and must be a positive, whole integer.\nUse: `roll help` for usage.");
-            process::exit(1);
-        }
-        if die == 0 {
-            println!("Cannot roll a 0 sided die.");
-            process::exit(1);
-        } else {
-            Dice {
-                results: roll_multiple(die, 1),
-                die: die,
-            }
-        }
-}
-
-fn two_args(args: Vec<String>) -> Dice {
-    let die: u32;
-    let num: u32;
-
-    if let Ok(n) = args[2].trim().parse() {
-        die = n;
-    } else {
-        println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
-        process::exit(1);
-    }
-
-    if let Ok(n) = args[1].trim().parse() {
-        num = n;
-    } else {
-        println!("Usage: roll <number of dice> <type of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
-        process::exit(1);
-    }
-    if num == 0 || die == 0 {
-        println!("Cannot roll 0 dice or a 0 sided die.");
-        process::exit(1);
-    }
-    if num > 2 {
-        println!("Rolling {} D{}s", num, die);
-    } else {
-        println!("Rolling 1 D{}", die);
-    }
-    Dice {
-        results: roll_multiple(die, num),
-        die: die,
-    }
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        no_args().print_result()
-    } else if args.len() == 2 {
-        if args[1] == String::from("help"){
-            help_menu();
-            process::exit(1);
+    let cli = Cli::parse();
+    let result: Dice;
+    
+    if let Some(die) = cli.die {
+        result = Dice {
+            results: roll_multiple(die, cli.num),
+            die: die,
         }
-
-        one_arg(args).print_result();
-    } else if args.len() == 3 {
-        two_args(args).print_result();
     } else {
-        println!("Usage: roll <type of die> <number of die>\nArguments are optional and must be whole positive integers.\nUse: `roll help` for usage.");
+        result = no_args();
     }
+
+    result.print_result();
 }
